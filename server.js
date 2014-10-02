05817c6fcd55fd561e7ce8d7c8e8e44c6db72092
unknown <sgregory@gmail.com>
2014-10-02 10:39:15 -0500

    Added morgan and body-parser

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
-var express = require('express');
 var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
 var app = express();
 
 app.set('port', process.env.PORT || 3001);
-
-app.get('/', function(req, res) {
-    res.json({ message: 'You are alive!' });
-});
-
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(app.get('port'), function() {
