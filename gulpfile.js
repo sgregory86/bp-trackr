e636f5ff56858abaf8f7e006affb9ff0b35a7cb9
unknown <sgregory@gmail.com>
2014-10-02 12:38:40 -0500

    Added Gulp, SASS & min tasks

---
+++ gulpfile.js
@@ -0,0 +1,18 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var csso = require('gulp-csso');
+var plumber = require('gulp-plumber');
+
+gulp.task('sass', function() {
+    gulp.src('public/css/master.scss')
+      .pipe(plumber())
+      .pipe(sass())
+      .pipe(csso())
+      .pipe(gulp.dest('public/css'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('public/css/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
