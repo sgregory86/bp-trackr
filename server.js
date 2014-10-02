e3e303fd110d6c29ef2532c50207bbd2d47f585d
unknown <sgregory@gmail.com>
2014-10-02 10:23:51 -0500

    Getting things started with node and express

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var path = require('path');
+var app = express();
+
+app.set('port', process.env.PORT || 3001);
+
+app.get('/', function(req, res) {
+    res.json({ message: 'You are alive!' });
+});
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
+});
