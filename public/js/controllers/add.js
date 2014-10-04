550527ebbb1f6fd44ff76a396c55fcd6e3d423db
unknown <sgregory@gmail.com>
2014-10-04 15:30:35 -0500

    Added default date and moment via bower

--- public/js/controllers/add.js
+++ public/js/controllers/add.js
@@ -1,8 +1,16 @@
 angular.module('MyApp')
   .controller('AddCtrl', function($scope, $alert, BloodPressure) {
+
+    $scope.date = moment(new Date()).format("M/DD/YY");
+
+    $scope.setDate = function() {
+      $scope.selectedDate ? $scope.selectedDate : $scope.date;
+    };
+
     $scope.addReading = function() {
       BloodPressure.save({ selectedDate: $scope.selectedDate, systolic: $scope.systolic, diastolic: $scope.diastolic }).$promise
         .then(function() {
+          $scope.date = '';
           $scope.selectedDate = '';
           $scope.systolic = '';
           $scope.diastolic = '';
@@ -16,6 +24,7 @@ angular.module('MyApp')
           });
         })
         .catch(function(response) {
+          $scope.date = '';
           $scope.selectedDate = '';
           $scope.systolic = '';
           $scope.diastolic = '';
