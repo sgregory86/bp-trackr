6fbd7527ee8dd91fdf517b8b60824ca1145d9f90
unknown <sgregory@gmail.com>
2014-10-04 06:44:36 -0500

    Syntax clean up

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,13 @@
 angular.module('MyApp', ['ngResource', 'ngMessages', 'ngRoute', 'mgcrea.ngStrap'])
   .config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-      $locationProvider.html5Mode(true);
-      $routeProvider 
-        .when('/', {
-          templateUrl: 'js/views/home.html',
-          controller: 'MainCtrl'
-        })
-        .otherwise({
-          redirectTo: '/'
-        });
+    $locationProvider.html5Mode(true);
+    $routeProvider 
+      .when('/', {
+        templateUrl: 'js/views/home.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
     }
   ]);
