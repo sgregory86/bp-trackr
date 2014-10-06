6ba3101950d4c96fa91d64dd64f9d4ca9e2be98b
unknown <sgregory@gmail.com>
2014-10-06 00:56:00 -0500

    Added update to UI on delete

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -3,5 +3,9 @@ angular.module('MyApp')
     function($scope, BloodPressure) {
       $scope.readingLimit = -5;
       $scope.readings = BloodPressure.query();
+      $scope.removeItem = function(reading) {
+        $scope.readings.splice($scope.readings.indexOf(reading), 1);
+        reading.$delete(reading);
+      };
     }
   ]);
