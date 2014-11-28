6c8f2b0f8bbc238c988e1790a18d05f83752deda
unknown <sgregory@gmail.com>
2014-11-28 12:08:32 -0600

    Moved routes into its own module

---
+++ routes/index.js
@@ -0,0 +1,45 @@
+var express = require('express'),
+    browserify = require('browserify'),
+    BloodPressure = require('.././public/models/bloodPressure'),
+    router = express.Router();
+
+router.get('/api/readings', function(req, res, next) {
+    var query = BloodPressure.find();
+    query.exec(function(err, readings) {
+        if (err) return next(err);
+        res.send(readings);
+    });
+});
+
+router.get('/api/readings/:id', function(req, res, next) {
+    BloodPressure.findById(req.params.id, function(err, reading) {
+        if (err) return next(err);
+        res.send(reading);
+    });
+});
+
+router.post('/api/readings', function(req, res, next) {
+    var bloodPressureTest = new BloodPressure({
+        date: req.body.selectedDate,
+        time: req.body.selectedTime,
+        systolic: req.body.systolic,
+        diastolic: req.body.diastolic
+    });
+    bloodPressureTest.save(function(err) {
+        if (err) return next(err);
+        res.send(200);
+    });
+});
+
+router.delete('/api/readings/:id', function(req, res, next) {
+    BloodPressure.findByIdAndRemove(req.params.id, function(err, reading) {
+        if (err) return next(err);
+        res.send(reading);
+    });
+});
+
+router.get('*', function(req, res) {
+    res.redirect('/#' + req.originalUrl);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,8 +3,7 @@ var path = require('path'),
     bodyParser = require('body-parser'),
     logger = require('morgan'),
     mongoose = require('mongoose'),
-    browserify = require('browserify'),
-    BloodPressure = require('./public/models/bloodPressure');
+    routes = require('./routes/index');
 
 mongoose.connect('mongodb://localhost:27017/bp');
 
@@ -16,44 +15,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/api/readings', function(req, res, next) {
-    var query = BloodPressure.find();
-    query.exec(function(err, readings) {
-        if (err) return next(err);
-        res.send(readings);
-    });
-});
-
-app.get('/api/readings/:id', function(req, res, next) {
-    BloodPressure.findById(req.params.id, function(err, reading) {
-        if (err) return next(err);
-        res.send(reading);
-    });
-});
-
-app.post('/api/readings', function(req, res, next) {
-    var bloodPressureTest = new BloodPressure({
-        date: req.body.selectedDate,
-        time: req.body.selectedTime,
-        systolic: req.body.systolic,
-        diastolic: req.body.diastolic
-    });
-    bloodPressureTest.save(function(err) {
-        if (err) return next(err);
-        res.send(200);
-    });
-});
-
-app.delete('/api/readings/:id', function(req, res, next) {
-    BloodPressure.findByIdAndRemove(req.params.id, function(err, reading) {
-        if (err) return next(err);
-        res.send(reading);
-    });
-});
-
-app.get('*', function(req, res) {
-    res.redirect('/#' + req.originalUrl);
-});
+app.use('/', routes);
 
 app.use(function(err, req, res, next) {
     console.error(err.stack);
