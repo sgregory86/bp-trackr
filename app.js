56c1a914d0e3a6a77d2a71ba0bad12f84dc36dae
unknown <sgregory@gmail.com>
2014-11-28 12:53:23 -0600

    Added updated app module

---
+++ app.js
@@ -0,0 +1,26 @@
+var path = require('path'),
+    express = require('express'),
+    bodyParser = require('body-parser'),
+    logger = require('morgan'),
+    mongoose = require('mongoose'),
+    routes = require('./routes/index');
+
+mongoose.connect('mongodb://localhost:27017/bp');
+
+var app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+
+app.use(function(err, req, res, next) {
+    console.error(err.stack);
+    res.send(500, {
+        message: err.message
+    });
+});
+
+module.exports = app;
