5204414ae16d74cb868e5f9c204c8e3a7c8eb6e7
Steven Gregory <sgregory@gmail.com>
2016-05-25 21:01:42 -0500

    Reformatting for consistency

--- src/client/app/add/add.controller.ts
+++ src/client/app/add/add.controller.ts
@@ -1,12 +1,6 @@
 namespace app.add {
     'use strict';
 
-    angular
-        .module('app.add')
-        .controller('Add', Add);
-
-    Add.$inject = ['$alert', 'config', 'dataservice', 'reading'];
-
     function Add($alert: mgcrea.ngStrap.alert.IAlertService, config: any, dataservice: any, reading: any) {
         var vm = this;
         var bloodPressure = dataservice.getReadings();
@@ -16,41 +10,45 @@ namespace app.add {
 
         function addReading(): void {
             bloodPressure.save({
-                    selectedDate: reading.setDateTime(vm.selectedDate, config.dateFormat),
-                    selectedTime: reading.setDateTime(vm.selectedTime, config.timeFormat),
-                    systolic: vm.systolic,
-                    diastolic: vm.diastolic
-                }).$promise
-                .then(function() {
-                    vm.date = '';
-                    vm.time = '';
-                    vm.selectedDate = '';
-                    vm.selectedTime = '';
-                    vm.systolic = '';
-                    vm.diastolic = '';
-                    $alert({
-                        content: 'Blood pressure has been added.',
-                        animation: 'fadeZoomFadeDown',
-                        placement: 'top-right',
-                        type: 'info',
-                        duration: 3
-                    });
-                })
-                .catch(function(response: any) {
-                    vm.date = '';
-                    vm.time = '';
-                    vm.selectedDate = '';
-                    vm.selectedTime = '';
-                    vm.systolic = '';
-                    vm.diastolic = '';
-                    $alert({
-                        content: response.data.message,
-                        animation: 'fadeZoomFadeDown',
-                        placement: 'top-right',
-                        type: 'info',
-                        duration: 3
-                    });
+                selectedDate: reading.setDateTime(vm.selectedDate, config.dateFormat),
+                selectedTime: reading.setDateTime(vm.selectedTime, config.timeFormat),
+                systolic: vm.systolic,
+                diastolic: vm.diastolic
+            }).$promise.then(function() {
+                vm.date = '';
+                vm.time = '';
+                vm.selectedDate = '';
+                vm.selectedTime = '';
+                vm.systolic = '';
+                vm.diastolic = '';
+                $alert({
+                    content: 'Blood pressure has been added.',
+                    animation: 'fadeZoomFadeDown',
+                    placement: 'top-right',
+                    type: 'info',
+                    duration: 3
                 });
+            }).catch(function(response: any) {
+                vm.date = '';
+                vm.time = '';
+                vm.selectedDate = '';
+                vm.selectedTime = '';
+                vm.systolic = '';
+                vm.diastolic = '';
+                $alert({
+                    content: response.data.message,
+                    animation: 'fadeZoomFadeDown',
+                    placement: 'top-right',
+                    type: 'info',
+                    duration: 3
+                });
+            });
         }
     }
+
+    Add.$inject = ['$alert', 'config', 'dataservice', 'reading'];
+
+    angular
+        .module('app.add')
+        .controller('Add', Add);
 }
--- src/client/app/app.route.ts
+++ src/client/app/app.route.ts
@@ -1,27 +1,24 @@
 namespace app {
     'use strict';
 
-    angular
-        .module('app')
-        .config(routeConfig);
+    function routeConfig($locationProvider: ng.ILocationProvider, $routeProvider: ng.route.IRouteProvider) {
+        $locationProvider.html5Mode(true);
+        $routeProvider.when('/', {
+            templateUrl: 'app/dashboard/dashboard.html',
+            controllerAs: 'vm',
+            controller: 'Dashboard'
+        }).when('/add', {
+            templateUrl: 'app/add/add.html',
+            controllerAs: 'vm',
+            controller: 'Add'
+        }).otherwise({
+            redirectTo: '/'
+        });
+    }
 
     routeConfig.$inject = ['$locationProvider', '$routeProvider'];
 
-    function routeConfig($locationProvider: ng.ILocationProvider, $routeProvider: ng.route.IRouteProvider) {
-        $locationProvider.html5Mode(true);
-        $routeProvider
-            .when('/', {
-                templateUrl: 'app/dashboard/dashboard.html',
-                controllerAs: 'vm',
-                controller: 'Dashboard'
-            })
-            .when('/add', {
-                templateUrl: 'app/add/add.html',
-                controllerAs: 'vm',
-                controller: 'Add'
-            })
-            .otherwise({
-                redirectTo: '/'
-            });
-    }
+    angular
+        .module('app')
+        .config(routeConfig);
 }
