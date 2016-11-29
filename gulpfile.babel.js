8cb1af50995b4d52a4d7b841993ae4f3b2f16db9
Steven Gregory <sgregory@gmail.com>
2016-11-29 06:13:37 -0600

    Added gulp yaml config and removed bump task

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,14 +3,16 @@
 import gulp from 'gulp';
 import sass from 'gulp-sass';
 import uglify from 'gulp-uglify';
-import bump from 'gulp-bump';
-import browserify from 'browserify';
-import source from 'vinyl-source-stream';
 import ts from 'gulp-typescript';
 import tslint from 'gulp-tslint';
+import browserify from 'browserify';
+import fs from 'file-system';
+import yaml from 'js-yaml';
+import source from 'vinyl-source-stream';
 import yargs from 'yargs';
 
 const args = yargs.argv;
+const config = yaml.safeLoad(fs.readFileSync('gulpfile.config.yml', 'utf8'));
 const tsProject = ts.createProject('tsconfig.json');
 
 gulp.task('sass', () => {
@@ -28,50 +30,18 @@ gulp.task('tslint', () => {
 });
 
 gulp.task('ts', ['tslint'], () => {
-  var sourceTsFiles = [
-    'typings/index.d.ts',
-    'src/client/app/app.module.ts',
-    'src/client/app/app.route.ts',
-    'src/client/app/core/core.module.ts',
-    'src/client/app/core/config.ts',
-    'src/client/app/core/data.service.ts',
-    'src/client/app/core/time.service.ts',
-    'src/client/app/dashboard/dashboard.module.ts',
-    'src/client/app/dashboard/dashboard.component.ts',
-    'src/client/app/dashboard/dashboard.interface.ts',
-    'src/client/app/dashboard/dashboard.controller.ts',
-    'src/client/app/reading/reading.module.ts',
-    'src/client/app/reading/reading.component.ts',
-    'src/client/app/reading/reading.interface.ts',
-    'src/client/app/reading/reading.controller.ts'
-  ];
-  var tsResult = gulp.src(sourceTsFiles)
+  let tsResult = gulp.src(config.tsFiles)
     .pipe(ts(tsProject));
   return tsResult.js.pipe(gulp.dest('src/client/app'));
 });
 
 gulp.task('bundle', ['ts'], () => {
   browserify({
-      entries: [
-        'node_modules/angular/angular.js',
-        'node_modules/angular-strap/dist/angular-strap.js',
-        'node_modules/angular-strap/dist/angular-strap.tpl.js',
-        'node_modules/angular-resource/angular-resource.js',
-        'node_modules/angular-route/angular-route.js',
-        'src/client/app/output.js'
-      ]
+      entries: config.bundleFiles
     })
     .bundle()
     .pipe(source('bundle.js'))
     .pipe(gulp.dest('src/client/app'));
-});
-
-gulp.task('bump', () => {
-  return gulp.src(['./bower.json', './package.json'])
-    .pipe(bump({
-      version: args.version
-    }))
-    .pipe(gulp.dest('./'));
 });
 
 gulp.task('watch', () => {
